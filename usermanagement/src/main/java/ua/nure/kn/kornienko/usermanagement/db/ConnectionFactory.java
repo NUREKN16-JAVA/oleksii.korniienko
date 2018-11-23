c8ee9597b071e934852aaca10db721e63589342e
oleksiikorniienko <oleksii.korniienko1@nure.ua>
2018-11-23 12:39:58 +0200

    connectionFactory and connection factory impl

---
+++ usermanagement/src/main/java/ua/nure/kn/kornienko/usermanagement/db/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package main.java.ua.nure.kn.kornienko.usermanagement.db;
+
+import java.sql.Connection;
+
+public interface ConnectionFactory {
+
+    Connection createConnection() throws DatabaseException;
+}
---
+++ usermanagement/src/main/java/ua/nure/kn/kornienko/usermanagement/db/ConnectionFactoryImpl.java
@@ -0,0 +1,13 @@
+package main.java.ua.nure.kn.kornienko.usermanagement.db;
+
+import java.sql.Connection;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+
+    @Override
+    public Connection createConnection() throws DatabaseException {
+
+        return null;
+    }
+
+}
